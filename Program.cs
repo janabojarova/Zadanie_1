ce248fb5dc2cd4d2b54810cda222d61ee8c9dfb4
Jana Bojarova <thejana13@gmail.com>
2022-03-30 17:52:38 +0300

    zadanie 1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace userdate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your PINcode");
+            string PINcode = Console.ReadLine();
+
+            if (PINcode == "1234")
+            {
+                Console.WriteLine("Hello, what is your name?");
+                string name = Console.ReadLine();
+                Console.WriteLine("Hello {0}", name );
+
+
+            }else
+                Console.WriteLine("Invalid PIN");
+
+
+
+            { 
+
+
+
+            }
+
+
+        }
+    }
+}
